a901e58178d119b53307b9619ded6558530834f2
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-02-21 00:03:49 +0500

    Install Packages

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import {
+    Navigate,
+    Routes,
+    Route
+} from "react-router-dom";
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Profile from './Pages/Profile';
+// import { useMemo } from "react";
+// import { useSelector } from "react-redux";
+// import { CssBaseline, ThemeProvider } from "@mui/material";
+// import { createTheme } from "@mui/material/styles";
+// import { themeSettings } from "./theme.js";
 
 const App = () => {
+
+    // const mode = useSelector((state) => state.mode);
+
+    // const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+    // const isAuth = Boolean(useSelector((state) => state.token));
+
     return (
-        <div>
+        <div className="app">
+            {/* <ThemeProvider theme={theme}> */}
+                {/* <CssBaseline /> */}
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/home" element={<Login />} />
+                    <Route path="/profile/:userId" element={<Profile />} />
+                </Routes>
+            {/* </ThemeProvider> */}
         </div>
     )
 };
---
+++ Client/src/Pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            home page 
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero perferendis minima quia perspiciatis ab illo eaque, earum, adipisci officiis omnis dignissimos accusantium ipsum voluptatum quibusdam? Numquam doloremque aliquid eveniet perspiciatis?
+        </div>
+    )
+};
+
+export default Home;
