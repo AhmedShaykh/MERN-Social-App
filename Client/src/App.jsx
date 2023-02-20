fbe6247ef5316f3a6055aa6bc1c5637cf8014a8e
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-02-21 00:15:57 +0500

    Add Reducer

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -7,30 +7,30 @@ import {
 import Home from './Pages/Home';
 import Login from './Pages/Login';
 import Profile from './Pages/Profile';
-// import { useMemo } from "react";
-// import { useSelector } from "react-redux";
-// import { CssBaseline, ThemeProvider } from "@mui/material";
-// import { createTheme } from "@mui/material/styles";
-// import { themeSettings } from "./theme.js";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme.js";
 
 const App = () => {
 
-    // const mode = useSelector((state) => state.mode);
+    const mode = useSelector((state) => state.mode);
 
-    // const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+    const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
-    // const isAuth = Boolean(useSelector((state) => state.token));
+    const isAuth = Boolean(useSelector((state) => state.token));
 
     return (
         <div className="app">
-            {/* <ThemeProvider theme={theme}> */}
-                {/* <CssBaseline /> */}
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
                 <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/home" element={<Login />} />
+                    <Route path="/" element={<Login />} />
+                    <Route path="/home" element={<Home />} />
                     <Route path="/profile/:userId" element={<Profile />} />
                 </Routes>
-            {/* </ThemeProvider> */}
+            </ThemeProvider>
         </div>
     )
 };
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -3,44 +3,44 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-// import { authSlice } from './State/Reducer.js';
-// import { configureStore } from "@reduxjs/toolkit";
-// import { Provider } from "react-redux";
-// import {
-//   persistStore,
-//   persistReducer,
-//   FLUSH,
-//   REHYDRATE,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-// } from "redux-persist";
-// import storage from "redux-persist/lib/storage";
-// import { PersistGate } from "redux-persist/integration/react";
+import authReducer from './State/Reducer.js';
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
 
-// const persistConfig = { key: "root", storage, version: 1 };
+const persistConfig = { key: "root", storage, version: 1 };
 
-// const persistedReducer = persistReducer(persistConfig, authSlice);
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
-// const store = configureStore({
-//   reducer: persistedReducer,
-//   middleware: (getDefaultMiddleware) =>
-//     getDefaultMiddleware({
-//       serializableCheck: {
-//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//       },
-//     }),
-// });
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <Provider store={store}>
-      <PersistGate loading={null} persistor={persistStore(store)}> */}
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      {/* </PersistGate>
-    </Provider> */}
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
