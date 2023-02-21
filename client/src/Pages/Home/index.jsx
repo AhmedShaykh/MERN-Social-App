3d61f9bf86bf0f0148c824afda459e96a4aca114
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-02-21 15:19:23 +0500

    Add Client

---
+++ client/src/Pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box } from "@mui/material";
+
+const Home = () => {
+    return (
+        <Box>
+            <h1>Hello</h1>
+        </Box>
+    )
+};
+
+export default Home;
